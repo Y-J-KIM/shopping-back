d709344b8945d426d05fd6c155558df58cdc6c04
Y-J-KIM <dudwn717@naver.com>
2024-08-20 21:57:37 +0900

    게시판 수정, config 추가

--- src/main/java/com/mysite/shoppingback/Controller/BoardController.java
+++ src/main/java/com/mysite/shoppingback/Controller/BoardController.java
@@ -26,96 +26,16 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BoardController {
 
-    @Value("${org.zerock.upload.path}")
+    @Value("${com.mysite.upload.path}")
     private String uploadPath;
 
     private final BoardService boardService;
-
-//    @GetMapping("/list")
-//    public void list(PageRequestDTO pageRequestDTO, Model model) {
-//        //controller에서 리턴이 void 일때 요청주소 /board/list와 같은 template html이 표시됨
-//        PageResponseDTO<BoardListAllDTO> responseDTO =
-//                boardService.listWithAll(pageRequestDTO);
-//        log.info(responseDTO);
-//        model.addAttribute("responseDTO", responseDTO);//화면에 전달
-//    }
 
     //@PreAuthorize("hasRole('USER')")
     @GetMapping("/register")
     public void register() {
 
     }
-
-//    @PostMapping("/register")
-//    public String registerPost(@Valid BoardDTO boardDTO,
-//                               BindingResult bindingResult,
-//                               RedirectAttributes redirectAttributes) {
-//        log.info("board Post register......");
-//        if (bindingResult.hasErrors()) {
-//            log.error("errors.....");
-//            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
-//            return "redirect:/board/register";
-//        }
-//        log.info(boardDTO.toString());
-//
-//        Long bno = boardService.register(boardDTO);
-//        redirectAttributes.addFlashAttribute("result", bno);
-//
-//        return "redirect:/board/list";
-//    }
-//
-//    //board/read.html , board/modify.html
-//    @PreAuthorize("isAuthenticated()") //인증된 유저만 게시글 읽기, 수정하기 가능
-//    @GetMapping({"/read","/modify"})
-//    public void read(Long bno, PageRequestDTO pageRequestDTO, Model model) {
-//        BoardDTO boardDTO = boardService.readOne(bno);
-//        log.info(boardDTO.toString());
-//        model.addAttribute("dto", boardDTO);
-//    }
-//
-//    @PreAuthorize("principal.username == #boardDTO.writer")
-//    @PostMapping("/modify")
-//    public String modify(PageRequestDTO pageRequestDTO,
-//                         @Valid BoardDTO boardDTO,
-//                         BindingResult bindingResult,
-//                         RedirectAttributes redirectAttributes) {
-//        log.info("board Modify Post......");
-//        String link = pageRequestDTO.getLink(); //페이지 유지
-//        long bno = boardDTO.getBno();
-//        link = link + "&bno=" + bno; //링크에 bno 추가
-//
-//        if(bindingResult.hasErrors()) {
-//            log.error("errors.....");
-//            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
-//            redirectAttributes.addFlashAttribute("bno", bno);
-//            //유효성검사 에러시 다시 수정페이지로 돌아감
-//            return "redirect:/board/modify?"+link;
-//        }
-//        boardService.modify(boardDTO);
-//        redirectAttributes.addFlashAttribute("result","modified");
-//        redirectAttributes.addFlashAttribute("bno", bno);
-//        return "redirect:/board/read?"+link; //수정후 읽기 페이지로 이동
-//    }
-//
-//    @PreAuthorize("principal.username == #boardDTO.writer")
-//    @PostMapping("/remove")
-//    public String remove(BoardDTO boardDTO, RedirectAttributes redirectAttributes) {
-//        Long bno = boardDTO.getBno();
-//        log.info("remove post.."+ bno);
-//
-//        boardService.remove(bno);
-//
-//        //게시물이 데이터베이스상에서 삭제되었다면 첨부파일 삭제
-//        log.info(boardDTO.getFileNames());
-//        List<String> fileNames = boardDTO.getFileNames();
-//        if(fileNames != null && fileNames.size() > 0) {
-//            removeFiles(fileNames);
-//        }
-//
-//        redirectAttributes.addFlashAttribute("result","removed");
-//
-//        return "redirect:/board/list";
-//    }
 
     // 게시글 목록 조회 (페이징, 검색 포함)
     @GetMapping("/list")
@@ -157,7 +77,7 @@ public class BoardController {
     }
 
     private String saveImage(MultipartFile image) {
-        String uploadDir = "path/to/upload/dir";
+        String uploadDir = "C:/upload";
         String originalFilename = image.getOriginalFilename();
         String filePath = Paths.get(uploadDir, originalFilename).toString();
 
--- src/main/java/com/mysite/shoppingback/ShoppingBackApplication.java
+++ src/main/java/com/mysite/shoppingback/ShoppingBackApplication.java
@@ -2,8 +2,10 @@ package com.mysite.shoppingback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //자동 등록,수정 시간 업데이트
 public class ShoppingBackApplication {
 
     public static void main(String[] args) {
